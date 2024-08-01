1da8b2dcf19f329ea4a4dc81275a0b5ed95b7142
Nguyen Hoa Binh <steven.nguyenhoabinh@gmail.com>
2024-08-01 12:49:33 +0700

    Fix layout bug

--- src/app/storage/[[...path]]/page.tsx
+++ src/app/storage/[[...path]]/page.tsx
@@ -3,7 +3,7 @@ import TopBar from '@/components/top_bar/TopBar';
 
 export default function Storage({ params }: IParams) {
 	return (
-		<main className="h-dvh bg-customBlack p-4 max-md:p-2 space-y-4">
+		<main className="min-h-dvh bg-customBlack p-4 max-md:p-2 space-y-4">
 			<TopBar directory={params.path ?? []} />
 
 			<MainContent path={params.path?.join('/') ?? ''} />
