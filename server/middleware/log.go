2a54d8170a999bf10c07bdc86c402b1612df13df
Nguyen Hoa Binh <steven.nguyenhoabinh@gmail.com>
2025-02-02 13:12:10 +0700

    Change log middleware

--- server/middleware/log.go
+++ server/middleware/log.go
@@ -10,15 +10,13 @@ import (
 
 const MAX_ID = 10000
 
-func Log(next http.Handler) http.Handler {
+func SetLog(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		id := rand.Intn(MAX_ID)
 		logger.InfoLogger.Printf("%s: %s (id: %d)", r.Method, r.URL.Path, id)
-		logger.InfoLogger.Print(r.Header)
 		startTime := time.Now()
 		next.ServeHTTP(w, r)
 		elapsedTime := time.Since(startTime)
-		logger.InfoLogger.Print(w.Header())
 		logger.InfoLogger.Printf("Response %d in %s", id, elapsedTime)
 	})
 }
