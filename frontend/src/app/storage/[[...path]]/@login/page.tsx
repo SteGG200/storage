c257f9520c63c8cedcc87f7432f1d2f750c69b37
Nguyễn Hòa Bình <steven.nguyenhoabinh@gmail.com>
2025-07-14 08:33:02 +0700

    feat: add a button for user to back to parent folder in login page

--- frontend/src/app/storage/[[...path]]/@login/page.tsx
+++ frontend/src/app/storage/[[...path]]/@login/page.tsx
@@ -1,12 +1,16 @@
 import LoginForm from "@/components/LoginForm";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 export default async function LoginInterface({
 	params
 }: {
 	params: Promise<{path?: string[]}>
 }) {
-	const path = ((await params).path ?? []).join('/')
+	const arrayPath = (await params).path
+	const path = (arrayPath ?? []).join('/')
 
 	return (
 		<div className="min-h-dvh flex items-center justify-center p-4">
@@ -15,8 +19,18 @@ export default async function LoginInterface({
 					<CardTitle className="text-2xl font-bold text-gray-100">Storage</CardTitle>
 					<CardDescription className="text-gray-400">Enter your password to access this folder</CardDescription>
 				</CardHeader>
-				<CardContent>
+				<CardContent className="space-y-4">
 					<LoginForm path={path}/>
+					{arrayPath && (
+						<div className="border-t border-gray-600 pt-4">
+							<Button variant="outline" className="w-full" asChild>
+								<Link className="text-white" href={`/storage/${arrayPath.slice(0, arrayPath.length - 1).join('/')}`}>
+									<ArrowLeft/>
+									Back
+								</Link>
+							</Button>
+						</div>
+					)}
 				</CardContent>
 			</Card>
 		</div>
